b5fb28fa821a2e0cfa19d03accea2538591e3d41
Logan Ferrera <loganferrera1234@gmail.com>
2022-07-06 20:35:58 -0700

    Responsive layout fixes for index and greatmen

---
+++ js/resolutions.js
@@ -0,0 +1,22 @@
+const ld = () => {
+  const width =
+  window.innerWidth
+const height =
+  window.innerHeight
+
+const sword = document.getElementById("Layer_1");
+if (width <= 2200) {
+  if (height <= 1000) {
+
+  } else {
+  }
+} else {
+}
+
+console.log(height)
+console.log(width)
+console.log(sword)
+console.log(sword.getAttribute('viewBox'))
+
+}
+ld()
