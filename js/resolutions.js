ffad4afb7b9963e0941c76c1bd7c93a7d9bae3c8
Logan Ferrera <loganferrera1234@gmail.com>
2022-07-09 07:34:31 -0700

    Add responsive js for sword svg. Overflow hidden

--- js/resolutions.js
+++ js/resolutions.js
@@ -5,18 +5,18 @@ const height =
   window.innerHeight
 
 const sword = document.getElementById("Layer_1");
-if (width <= 2200) {
-  if (height <= 1000) {
-
+if (width <= 380 ) {
+  if (height <= 670) {
+    sword.removeAttribute('viewBox')
+    sword.setAttribute('viewBox', '11 0 1000 429')
   } else {
+    sword.setAttribute('viewBox', '-11 170 1000 429')
   }
 } else {
-}
-
-console.log(height)
-console.log(width)
-console.log(sword)
-console.log(sword.getAttribute('viewBox'))
+  sword.setAttribute('viewBox', '-11 170 1000 429')
 
 }
-ld()
+
+}
+
+addEventListener('resize', (event) => {ld()});
